b2cd780a6184b7ea397289c21104808074538134
Josh Cesana <joshuacesana@gmail.com>
2018-07-09 20:43:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,7 +40,8 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end 
   
-  def self.create(student_hash)
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
     
   end 
 end
